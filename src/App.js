a7cee2b03c7afbabcacd0edb571aea2702b28d24
BharathSharath <103111105+BharathSharath@users.noreply.github.com>
2022-09-22 18:41:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-
-          Good Evening Everyone!!!.
+          Edit <code>src/App.js</code> and save to reload.!! ok Webhook is working
         </p>
         <a
           className="App-link"
