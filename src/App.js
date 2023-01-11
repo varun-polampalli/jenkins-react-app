c666310400cdbaf51d33c3ab2c694dde3bcece30
varun-polampalli <118754117+varun-polampalli@users.noreply.github.com>
2023-01-11 15:47:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.!! Okay skillrary
+          Edit <code>src/App.js</code> and save to reload.!! okay ajay!!!!!!!
         </p>
         <a
           className="App-link"
